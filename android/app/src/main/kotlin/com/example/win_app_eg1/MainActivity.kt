69491b1930faa23101bc7d91fd36e5c712cb0207
faizza.mohammed <faizza.mohammed@gmail.com>
2022-04-01 16:16:05 +0530

    .

---
+++ android/app/src/main/kotlin/com/example/win_app_eg1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.win_app_eg1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/text_delegate.dart
@@ -0,0 +1,45 @@
+import 'package:image_painter/image_painter.dart';
+
+class EngTextDelegate implements TextDelegate {
+  @override
+  String get arrow => "arrow";
+
+  @override
+  String get changeBrushSize => "changeBrushSize";
+
+  @override
+  String get changeColor => "changeColor";
+
+  @override
+  String get changeMode => "changeMode";
+
+  @override
+  String get circle => "circle";
+
+  @override
+  String get clearAllProgress => "clearAllProgress";
+
+  @override
+  String get dashLine => "dashLine";
+
+  @override
+  String get done => "done";
+
+  @override
+  String get drawing => "pen";
+
+  @override
+  String get line => "line";
+
+  @override
+  String get noneZoom => "noneZoom";
+
+  @override
+  String get rectangle => "square";
+
+  @override
+  String get text => "text";
+
+  @override
+  String get undo => "undo";
+}
